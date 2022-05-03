9cbe1477c44feb688a098e5f0a5114cc32a0dd8b
jojofront <jojofront@debian.devops.lan>
2022-05-03 14:26:54 +0200

    add dockerfile + docker compose

--- westerosDictator/election/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Regime, Dictateur,Election
-
-# Register your models here.
-admin.site.register(Regime)
-admin.site.register(Dictateur)
-# admin.site.register(Paysan)
-admin.site.register(Election)
